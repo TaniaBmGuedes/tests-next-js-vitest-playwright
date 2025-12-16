54af0b08b8a8088c5090b66b57f7f6d5b95c27e9
taniabmguedes@gmail.com <taniabmguedes@gmail.com>
2025-12-16 09:59:07 +0000

    fix a bug

--- src/core/todo/factories/repositories/drizzle-todo.repository.test.ts
+++ src/core/todo/factories/repositories/drizzle-todo.repository.test.ts
@@ -1,126 +1,44 @@
-import { insertTestTodos, makeTestTodoRepository } from '@/core/_test_/utils/make-test-todo-repository';
 
+import { drizzleDatabase } from '@/db/drizzle';
+import { eq } from 'drizzle-orm';
+import { DrizzleTodoRepository } from './drizzle-todo.repository';
 
-describe('DrizzleTodoRepository (integration)', () => {
-  beforeEach(async () => {
-    const { deleteTodoNoWhere } = await makeTestTodoRepository();
-    await deleteTodoNoWhere();
+export async function makeTestTodoRepository() {
+  const { db, todoTable } = drizzleDatabase;
+  const repository = new DrizzleTodoRepository(db);
+  const todos = makeTestTodos();
+
+  const insertTodoDb = () => db.insert(todoTable);
+  const deleteTodoNoWhere = () => db.delete(todoTable);
+  const deleteTodoDb = (id: string) =>
+    db.delete(todoTable).where(eq(todoTable.id, id));
+
+  return {
+    todos,
+    repository,
+    insertTodoDb,
+    deleteTodoDb,
+    deleteTodoNoWhere,
+  };
+}
+
+export const insertTestTodos = async () => {
+  const { insertTodoDb } = await makeTestTodoRepository();
+  const todos = makeTestTodos();
+
+  await insertTodoDb().values(todos);
+
+  return todos;
+};
+
+export const makeTestTodos = () => {
+  return Array.from({ length: 5 }).map((_, index) => {
+    const newTodo = {
+      id: index.toString(),
+      description: `Todo ${index}`,
+      createdAt: `date ${index}`,
+    };
+
+    return newTodo;
   });
-
-  afterAll(async () => {
-    const { deleteTodoNoWhere } = await makeTestTodoRepository();
-    await deleteTodoNoWhere();
-  });
-
-  describe('findAll', () => {
-    test('returns an empty array when the table is clear', async () => {
-      const { repository } = await makeTestTodoRepository();
-      const result = await repository.findAll();
-      expect(result).toStrictEqual([]);
-      expect(result).toHaveLength(0);
-    });
-
-    test('returns all TODOs in descending order', async () => {
-      const { repository } = await makeTestTodoRepository();
-      await insertTestTodos();
-      const result = await repository.findAll();
-      expect(result[0].createdAt).toBe('date 4');
-      expect(result[1].createdAt).toBe('date 3');
-      expect(result[2].createdAt).toBe('date 2');
-      expect(result[3].createdAt).toBe('date 1');
-      expect(result[4].createdAt).toBe('date 0');
-    });
-  });
-
-  describe('create', () => {
-    test('creates a todo when the data is valid', async () => {
-      const { repository, todos } = await makeTestTodoRepository();
-      const newTodo = await repository.create(todos[0]);
-      expect(newTodo).toStrictEqual({
-        success: true,
-        todo: todos[0],
-      });
-    });
-
-    test('fails if a duplicate description exists in the table', async () => {
-      const { repository, todos } = await makeTestTodoRepository();
-
-      // Create a new todo
-      await repository.create(todos[0]);
-
-      // Attempt to create another todo with the same description
-      const anotherTodo = {
-        id: 'any id',
-        description: todos[0].description,
-        createdAt: 'any date',
-      };
-      const result = await repository.create(anotherTodo);
-
-      expect(result).toStrictEqual({
-        success: false,
-        errors: ['A todo with the provided id or description already exists'],
-      });
-    });
-
-    test('fails if a duplicate ID exists in the table', async () => {
-      const { repository, todos } = await makeTestTodoRepository();
-
-      // Create a new todo
-      await repository.create(todos[0]);
-
-      // Attempt to create another todo with the same ID
-      const anotherTodo = {
-        id: todos[0].id,
-        description: 'any description',
-        createdAt: 'any date',
-      };
-      const result = await repository.create(anotherTodo);
-
-      expect(result).toStrictEqual({
-        success: false,
-        errors: ['A todo with the provided id or description already exists'],
-      });
-    });
-
-    test('fails if both the ID and description are duplicates', async () => {
-      const { repository, todos } = await makeTestTodoRepository();
-
-      await repository.create(todos[0]);
-
-      const anotherTodo = {
-        id: todos[0].id,
-        description: todos[0].description,
-        createdAt: 'any date',
-      };
-      const result = await repository.create(anotherTodo);
-
-      expect(result).toStrictEqual({
-        success: false,
-        errors: ['A todo with the provided id or description already exists'],
-      });
-    });
-  });
-
-  describe('remove', () => {
-    test('deletes a todo if it exists', async () => {
-      const { repository, todos } = await makeTestTodoRepository();
-      await insertTestTodos();
-      const result = await repository.remove(todos[0].id);
-
-      expect(result).toStrictEqual({
-        success: true,
-        todo: todos[0],
-      });
-    });
-
-    test('fails to delete when the todo does not exist', async () => {
-      const { repository } = await makeTestTodoRepository();
-      const result = await repository.remove('any id');
-
-      expect(result).toStrictEqual({
-        success: false,
-        errors: ['Todo does not exist'],
-      });
-    });
-  });
-});
+};
