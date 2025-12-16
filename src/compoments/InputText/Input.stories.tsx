9a66bcb3c9e3cc5e154f6bd250a374963714cf66
taniabmguedes@gmail.com <taniabmguedes@gmail.com>
2025-12-16 14:04:05 +0000

    my form

--- src/compoments/InputText/Input.stories.tsx
+++ src/compoments/InputText/Input.stories.tsx
@@ -7,7 +7,7 @@ const meta: Meta<typeof InputText> = {
   component: InputText,
   decorators: [
     Story => (
-      <div className='max-w-screen-lg mx-auto p-12'>
+      <div className='max-w-5xl mx-auto p-12'>
         <Story />
       </div>
     ),
---
+++ src/compoments/TodoForm/TodoForm.stories.tsx
@@ -0,0 +1,49 @@
+
+import React from 'react';
+import { CreateTodoAction } from '@/core/todo/actions/todo.actions.types';
+import { TodoForm } from '.';
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+const meta: Meta<typeof TodoForm> = {
+  title: 'Components/Forms/TodoForm',
+  component: TodoForm,
+  decorators: [
+    Story => (
+      <div className='max-w-3xl mx-auto p-12'>
+        <Story />
+      </div>
+    ),
+  ],
+  argTypes: {
+    action: {
+      control: false,
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof TodoForm>;
+
+export const Default: Story = {
+  args: {
+    action: fn(async () => {
+      return {
+        success: true,
+        todo: { id: 'id', description: 'desc', createdAt: 'data' },
+      };
+    }) as CreateTodoAction,
+  },
+};
+
+export const WithError: Story = {
+  args: {
+    action: fn(async () => {
+      return {
+        success: false,
+        errors: ['fail to create todo'],
+      };
+    }) as CreateTodoAction,
+  },
+};
